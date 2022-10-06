f85dfaf6486089ebce0d65dad3916522048d9fc1
rvitality <vital.renzjohn@gmail.com>
2022-10-06 14:14:39 +0800

    Completed: Sum of Positive

---
+++ 8-kyus/sum_of_positive.py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/5715eaedb436cf5606000381/train/python
+
+# ! Sum of positive --------------
+
+def positive_sum(arr):
+    sum = 0;
+    for num in arr:
+        if(num > 0):
+            sum += num
+
+
+    return sum;
+
+
+
+
+
