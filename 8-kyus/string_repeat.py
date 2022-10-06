9ea1035ff30c8cdfee76f17f5484953bcf17e8aa
rvitality <vital.renzjohn@gmail.com>
2022-10-06 15:04:32 +0800

    Completed: String repeat

---
+++ 8-kyus/string_repeat.py
@@ -0,0 +1,23 @@
+# https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/python
+
+# ! String repeat --------------
+
+def repeat_str(repeat, string):
+    res = ""
+
+    for i in range(repeat):
+        res += string
+
+    return res
+
+    # "the pythonic way"
+    # return string * repeat;
+
+
+
+
+
+
+
+
+
