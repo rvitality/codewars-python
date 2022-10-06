eca087e2a3d1a487295ae52b56484c8fb831dd4a
rvitality <vital.renzjohn@gmail.com>
2022-10-06 14:40:55 +0800

    Completed: Convert boolean to strings 'Yes' or 'No'

---
+++ 8-kyus/convert_boolean_values_to_strings_yes_or_no.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/53369039d7ab3ac506000467/train/python
+
+# ! Convert boolean values to strings 'Yes' or 'No' --------------
+
+def bool_to_word(boolean):
+    return "Yes" if boolean else "No"
+
+
+
+
+
+
+
