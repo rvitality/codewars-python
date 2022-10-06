3d300170284e88babf7ec45a3cd3f70693cf19ec
rvitality <vital.renzjohn@gmail.com>
2022-10-06 14:30:09 +0800

    Completed: Reversed string

---
+++ 8-kyus/reversed_string.py
@@ -0,0 +1,18 @@
+# https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/python
+
+# ! Reverse String --------------
+
+def solution(string):
+    str = ""
+    for char in string:
+            str = char + str
+
+    return str
+
+
+
+
+
+
+
+
