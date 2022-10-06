d4e8c699214c1ad372f17b15b0d92f39d97e6241
rvitality <vital.renzjohn@gmail.com>
2022-10-06 23:08:06 +0800

    Completed: Square(n) Sum

---
+++ 8-kyus/square_n_sum.py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/515e271a311df0350d00000f/train/python
+
+# ! Square(n) Sum --------------
+
+def square_sum(numbers):
+    sum = 0;
+    for num in numbers:
+        sum += num ** 2
+
+    return sum
+
+
+
+
+
+
+
