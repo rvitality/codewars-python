18b4d714e90c24f6d73d0b11b90456e14334a556
rvitality <vital.renzjohn@gmail.com>
2022-10-06 12:19:22 +0800

    Completed: Even or Odd

---
+++ 8-kyus/even_or_odd.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/python
+
+# ! A Needle in the Haystack --------------
+def even_or_odd(number):
+    return "even" if number % 2 == 0 else "odd"
+
+
+
+
