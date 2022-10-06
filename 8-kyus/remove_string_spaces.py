3487bd1964bcb0316cb1122b6cfbe48839ce131f
rvitality <vital.renzjohn@gmail.com>
2022-10-06 23:03:23 +0800

    Completed: Remove string spaces

---
+++ 8-kyus/remove_string_spaces.py
@@ -0,0 +1,27 @@
+# https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/python
+
+# ! Remove String Spaces --------------
+
+# "the python way" 
+def no_space(x):
+    print(x.replace(" ", ""))
+
+
+# def no_space(str):
+#     res = ""
+
+#     for char in str:
+#         if(char == " "):
+#             continue
+#         else:
+#             res += char
+    
+#     return res
+
+
+
+
+
+
+
+
