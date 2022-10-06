6596fad320fd82aba1e86d17325d2e6ecb6718ca
rvitality <vital.renzjohn@gmail.com>
2022-10-06 14:17:07 +0800

    Completed: Return negative

---
+++ 8-kyus/return_negative.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/55685cd7ad70877c23000102/train/python
+
+# ! Sum of positive --------------
+
+def make_negative(number):
+    return  number * -1 if number > 0 else number
+
+
+
+
+
