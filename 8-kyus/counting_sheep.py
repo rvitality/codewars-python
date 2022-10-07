826dfc6d4ade51301bc1a43349122074c3430d48
rvitality <vital.renzjohn@gmail.com>
2022-10-07 09:06:30 +0800

    Completed: Counting sheep

---
+++ 8-kyus/counting_sheep.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/54edbc7200b811e956000556/train/python
+
+# ! Counting sheep... --------------
+
+def count_sheeps(sheep):
+    return len(list(filter(bool, sheep)))
+    # return sheep.count(True)
+
+
+
+
+
+
+
