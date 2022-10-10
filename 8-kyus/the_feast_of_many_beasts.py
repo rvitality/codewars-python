351ee1586402d64df062ca1259b994f2677ae213
rvitality <vital.renzjohn@gmail.com>
2022-10-10 20:19:59 +0800

    Completed: The feast of many beasts

---
+++ 8-kyus/the_feast_of_many_beasts.py
@@ -0,0 +1,11 @@
+#  https://www.codewars.com/kata/5aa736a455f906981800360d/train/python
+
+# ! The Feast of Many Beasts --------------
+
+def feast(beast, dish):
+    return beast[0] == dish[0] and dish[-1] ==beast[-1]
+
+
+
+
+
