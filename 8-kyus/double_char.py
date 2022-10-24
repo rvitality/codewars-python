de664f9d026a8a0909fdf4777635ece95e06851f
rvitality <vital.renzjohn@gmail.com>
2022-10-25 07:08:04 +0800

    Completed: Double Char

---
+++ 8-kyus/double_char.py
@@ -0,0 +1,5 @@
+# https://www.codewars.com/kata/56b1f01c247c01db92000076/solutions/python
+
+# ! Double Char --------------
+def double_char(string):
+    return "".join([char * 2 for char in string])
