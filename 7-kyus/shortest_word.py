e0f4b90a569002de7d26cbb43353a32cccd31239
rvitality <vital.renzjohn@gmail.com>
2022-10-31 08:27:01 +0800

    Completed: Shortest Word

---
+++ 7-kyus/shortest_word.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9/train/python
+
+# ! Shortest Word --------------
+
+
+def find_short(s):
+    return min([len(word) for word in s.split(" ")])
