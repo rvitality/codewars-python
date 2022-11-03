8e57b3242f9586e0733f761865ee5cf21687df53
rvitality <vital.renzjohn@gmail.com>
2022-11-03 12:37:31 +0800

    Completed: Is n divisible by x and y?

---
+++ 8-kyus/is_n_divisible_by_x_and_y.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/5545f109004975ea66000086/train/python
+
+# ! Is n divisible by x and y? --------------
+
+
+def is_divisible(n, x, y):
+    return n % x == 0 and n % y == 0
