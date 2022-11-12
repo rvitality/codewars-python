69eb150340126f83b470111d17ee2cd7570a1631
rvitality <vital.renzjohn@gmail.com>
2022-11-12 22:25:22 +0800

    Completed: A needle in the haystack

---
+++ 8-kyus/a_needle_in_the_haystack.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/train/python
+
+# ! A Needle in the Haystack --------------
+
+
+def find_needle(haystack):
+    return f"found the needle at position {haystack.index('needle')}"
