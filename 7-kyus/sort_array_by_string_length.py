3562f6907ed40cabd44c60a9173a3cf22faa42fe
rvitality <vital.renzjohn@gmail.com>
2022-11-14 11:00:18 +0800

    Completed: Sort array by string length

---
+++ 7-kyus/sort_array_by_string_length.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/57ea5b0b75ae11d1e800006c/train/python
+
+# ! Sort array by string length --------------
+
+
+def sort_by_length(arr):
+    return sorted(arr, key=len)
